9374402113bbb104c535e51e372b5b51079ec2b9
João Araújo <joaoaraujo.78@gmail.com>
2022-02-17 18:25:23 +0000

    Gui version

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ let number;
 let computerSel;
 let humanSel;
 let result;
-let round = 1;
 let computerWins = 0;
 let humanWins = 0;
-
 
 function computerPlay() {
     number = Math.floor(Math.random() * 3);
@@ -21,16 +19,6 @@ function computerPlay() {
             break;
     }
     return computerSel;
-}
-
-function humanPlay () {
-    // while (humanSel != 'rock' || humanSel != 'paper' || humanSel != 'scissor') {
-    //     humanSel = prompt("Choose wisely:");
-    //     humanSel = humanSel.toLowerCase();
-    //     }
-    // return humanSel;
-
-    humanSel = prompt("Choose wisely:").toLowerCase();
 }
 
 function playRound (computerVar, humanVar) {
@@ -51,25 +39,109 @@ function playRound (computerVar, humanVar) {
     }
 }
 
+document.getElementById('roche').addEventListener('click', () => {
+    humanSel = 'rock';
+    console.log(humanSel);
+    game();
+});
+
+document.getElementById('papier').addEventListener('click', () => {
+    humanSel = 'paper';
+    console.log(humanSel);
+    game();
+});
+
+document.getElementById('ciseaux').addEventListener('click', () => {
+    humanSel = 'scissor';
+    console.log(humanSel);
+    game();
+});
+
 function game () {
-    while (round <= 5) {
 
-        computerPlay();
-        humanPlay();
-        playRound(humanSel, computerSel);
+    const playResult = document.querySelector('.onGoing');
+    const displayResultHuman = document.querySelector('.humanPoints');
+    const displayResultPC = document.querySelector('.computerPoints');
 
+    computerPlay();
+    playRound(humanSel, computerSel);
+
+    
         if (result == 'Human wins!') {
             humanWins = humanWins + 1;
+            playResult.textContent = 'You win!';
         } else if (result == 'Computer wins!') {
             computerWins = computerWins + 1;
+            playResult.textContent = 'You lost!'
+        } else {
+            playResult.textContent = 'Tie!';
         }
 
-        console.log(result);
-        console.log(humanWins);
-        console.log(computerWins);
+        displayResultHuman.textContent = `Your score: ${humanWins}`;
+        displayResultPC.textContent = `Computer score: ${computerWins}`;
 
-        round = round + 1;
-    }
+
+    console.log(result);
+    console.log(humanWins);
+    console.log(computerWins);
+    
+    restartGame();
+
+    return playResult, displayResultHuman, displayResultPC;
 }
 
-game()
+
+function restartGame() {
+
+    if (computerWins == 5 || humanWins == 5) {
+        const container = document.querySelector('#game');
+        const postResult = document.createElement('div');
+        postResult.classList.add('resultAnoun');
+        
+        if (computerWins == 5) {
+            postResult.textContent = 'Computer Wins!';
+        } else if (humanWins == 5) {
+            postResult.textContent = 'Human Wins!';
+        }
+    
+        container.appendChild(postResult);
+
+        computerWins = 0;
+        humanWins = 0;   
+
+        const playAgain = document.createElement('div');
+        playAgain.textContent = 'Do you wanna play again?';
+
+        const yesButton = document.createElement('button');
+        const noButton = document.createElement('button');
+        yesButton.textContent = "Oui";
+        noButton.textContent = "Non";
+        yesButton.setAttribute('id', 'yes');
+        noButton.setAttribute('id', 'no');
+
+        container.appendChild(playAgain);
+        playAgain.appendChild(yesButton);
+        playAgain.appendChild(noButton);
+        
+        document.getElementById('yes').addEventListener('click', () => {
+            humanWins, computerWins = 0;
+            playAgain.remove();
+            yesButton.remove();
+            noButton.remove();
+            postResult.remove();
+            const displayResultHuman = document.querySelector('.humanPoints');
+            const displayResultPC = document.querySelector('.computerPoints');
+            displayResultHuman.textContent = `Your score: ${humanWins}`;
+            displayResultPC.textContent = `Computer score: ${computerWins}`;
+            const playResult = document.querySelector('.onGoing');
+            playResult.textContent = "";
+        });
+
+        document.getElementById('no').addEventListener('click', () => {
+            const thankYou = document.createElement('div');
+            thankYou.textContent = 'Merci! Vive la France';
+            thankYou.setAttribute('padding', '20px;');
+            container.appendChild(thankYou);
+        });
+    }
+}
